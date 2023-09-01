0a6273aef8acd6b1ffa6c3a0306fab6a6bab1b96
Max Leonov <115459710+ScaRS32@users.noreply.github.com>
2023-09-01 15:08:15 +0300

    Update widget2.php

--- widget2.php
+++ widget2.php
@@ -3,18 +3,17 @@ require 'vendor/autoload.php';
 
 require_once __DIR__ . '/src/AmoCrmV4Client.php';
 
-define('SUB_DOMAIN', 'alikson');
-define('CLIENT_ID', '60d0e22a-fb64-47f0-856e-05b7c0bfe2e5');
-define('CLIENT_SECRET', 'ByJeFXBo3eTM9WWUJHkXK4F1OfLCw3FPwshjTYhdv1JNJHWDwtU4QNa3868UtnBG');
-define('CODE', 'def50200e85064322469fc80b6995c5303f2a6488ecd865fe39525cc59159531938ef122627ea3d2991a9e951997d39b064bf5ae4db66344e2e5984da6be5e1c0b0e6859a2383560d95527a425346b3f39ed093e10534dc535779a89f4898dcba1f0e8d16afc8a52e1f40d6d626a8ebb66f54f0732acdb0c8264d9bdd3f14133c5dd210a3a78ea4d56d84ae94673737466cb698ca43d46b060e0e1723ae32e3a4ba91e8f1bb861a1b11acca52d81e04868ab831f3661a6059ed7ee944c8c71e479a7b349d97e43eb5c9ed544a46288708d55599a344ea8ee58320e9c16e3df11ddf8c1ad4010919572e2603af87ee6239beabd1e76934c9588d6e6ff9e028d2f1b82e066f2aa9215e9ca4f8bfb33610f7c14625a58617643759ad540ea2553214e0d1c9083e291eb8cfb33ce036cc2bccaf9c554f4a11aa9469588597b821d8e359d94bcc1db4610c1a47dd99dc4bb7eb1973626cf56de3de2c5e3af70d1cc0483eb7e215ea7a3581ca74fc7f7eb1a03a0924c65182e4a8189ef18181a80c51a0fe553288e3bc7a3320f4d38930a037a789c3e7b418666cf23d8ffa082267f56e63e50d5997eec98b4e09521e613fc93546d020c9576f533a5b77abca01348b3278227247c2526f59811deb495ce4e8e640ff1b2ee5d9a1a0351fa751da730ac683b82fc3f6fe7ee9d');
-define('REDIRECT_URL', 'https://alikson.amocrm.ru');
+define('SUB_DOMAIN', '');
+define('CLIENT_ID', '');
+define('CLIENT_SECRET', '');
+define('CODE', '');
+define('REDIRECT_URL', '');
 
 
 
 $responsibleUserIds = [9649906, 9649914, 9810786, 9823110, 9860838, 9900262, 9962898, 9963002];
 $sumsByManager = [];
 $managerNames = []; // Инициализация массива для имен менеджеров
-$telegram = new \TelegramBot\Api\BotApi('6098750394:AAEukzP6v28GowwvNoNFVZ5wsJJzVM1AJFQ');
 $uniqueLeadIds =[];
 $midnightUnixTime = strtotime('today');
 
